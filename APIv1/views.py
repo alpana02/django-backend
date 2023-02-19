7989b500166e08c828edb51b00200bd826b203c0
Alpana Nanda <64356997+alpana02@users.noreply.github.com>
2023-02-19 15:55:33 +0530

    done

--- APIv1/views.py
+++ APIv1/views.py
@@ -129,9 +129,8 @@ def generateSentencesPdf (url):
 #     url = url.replace("/","\\")
 #     print(url,"++++++++++++++++++++++++++")
 #     x = "media\\uploadedPdf\\Undertakingbystudents_v50m5Fb.pdf"
-    url = os.path.join(os.getcwd(),url)
+    url = os.path.join(os.getcwd(),url[1:])
     pdf = PdfReader(url)
-#     pdf = PdfReader(url[1:])
     text = []
     for page_num in range(len(pdf.pages)):
         pageObj = pdf.pages[page_num]
